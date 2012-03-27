a1612efee957b4900cc8e19ef88518b76f3da359
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-03-27 11:04:15 -0500

    - bootstrapping language install view
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- installation/views/language/tmpl/default.php
+++ installation/views/language/tmpl/default.php
@@ -7,33 +7,20 @@
 
 defined('_JEXEC') or die;
 ?>
-<div id="step">
-	<div class="far-right">
-	<?php if ($this->document->direction == 'ltr') : ?>
-		<div class="button1-left"><div class="next"><a href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-	<?php elseif ($this->document->direction == 'rtl') : ?>
-		<div class="button1-right"><div class="prev"><a href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a></div></div>
-	<?php endif; ?>
-	</div>
-	<h2><?php echo JText::_('INSTL_LANGUAGE_TITLE'); ?></h2>
-</div>
-<form action="index.php" method="post" id="adminForm" class="form-validate">
+<form action="index.php" method="post" id="adminForm" class="form-validate form-horizontal">
 	<div id="installer">
-		<div class="m">
-			<h3><?php echo JText::_('INSTL_SELECT_LANGUAGE_TITLE'); ?></h3>
-			<div class="install-text">
-				<?php echo JText::_('INSTL_SELECT_LANGUAGE_DESC'); ?>
-			</div>
-			<div class="install-body">
-				<div class="m">
-					<fieldset>
-						<?php echo $this->form->getInput('language'); ?>
-					</fieldset>
+		<fieldset>
+			<legend><?php echo JText::_('INSTL_LANGUAGE_TITLE'); ?></legend>
+			<div class="control-group">
+				<label for="jform_language" class="control-label"><?php echo JText::_('INSTL_SELECT_LANGUAGE_TITLE'); ?></label>
+				<div class="controls">
+					<?php echo $this->form->getInput('language'); ?>
 				</div>
-				<div class="clr"></div>
 			</div>
-			<div class="clr"></div>
-		</div>
+			<div class="form-actions">
+				<a href="#" class="btn btn-primary" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNext'); ?>"><?php echo JText::_('JNext'); ?></a>
+			</div>
+		</fieldset>
 	</div>
 	<input type="hidden" name="task" value="setup.setlanguage" />
 	<?php echo JHtml::_('form.token'); ?>
