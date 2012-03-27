731884f774f5486cc7d29d5ec5e3e1625b4ffa75
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-03-27 11:03:57 -0500

    - updating installation template for bootstrap
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- installation/template/index.php
+++ installation/template/index.php
@@ -13,12 +13,18 @@ $doc = JFactory::getDocument();
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 
 // Add Stylesheets
-$doc->addStyleSheet('../media/system/css/system.css');
+$doc->addStyleSheet('../media/system/css/bootstrap.css');
+$doc->addStyleSheet('../media/system/css/bootstrap-extended.css');
+$doc->addStyleSheet('../media/system/css/bootstrap-responsive.css');
 $doc->addStyleSheet('template/css/template.css');
 
 if ($this->direction == 'rtl') {
 	$doc->addStyleSheet('template/css/template_rtl.css');
 }
+
+// Add JavaScript
+$doc->addScript('../media/system/js/jquery.js');
+$doc->addScript('../media/system/js/bootstrap.js');
 
 // Load the JavaScript behaviors
 JHtml::_('behavior.framework', true);
@@ -27,7 +33,7 @@ JHtml::_('behavior.tooltip');
 JHtml::_('behavior.formvalidation');
 JHtml::_('script', 'installation/template/js/installation.js', true, false, false, false);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 	<head>
 		<jdoc:include type="head" />
@@ -46,36 +52,27 @@ JHtml::_('script', 'installation/template/js/installation.js', true, false, fals
 			});
  		</script>
 	</head>
-	<body>
-		<div id="header">
-			<span class="logo"><a href="http://www.joomla.org" target="_blank"><img src="template/images/logo.png" alt="Joomla!" /></a></span>
-			<h1>Joomla! <?php echo JVERSION; ?> <?php echo JText::_('INSTL_INSTALLATION') ?></h1>
-		</div>
-		<jdoc:include type="message" />
-		<div id="content-box">
-			<div id="content-pad">
-				<div id="stepbar">
-					<?php echo JHtml::_('installation.stepbar'); ?>
-					<div class="box"></div>
-				</div>
-				<div id="warning">
-					<noscript>
-						<div id="javascript-warning">
-							<?php echo JText::_('INSTL_WARNJAVASCRIPT'); ?>
-						</div>
-					</noscript>
-				</div>
-				<div id="right">
-					<div id="rightpad">
-						<jdoc:include type="installation" />
+	<body class="installation">
+		<!-- Header -->
+		<div class="header">
+			<div class="header-inner">
+				<div class="container"> 
+					<div class="brand">
+						<img src="<?php echo $this->baseurl ?>/template/images/joomla.png" alt="Joomla" />
 					</div>
 				</div>
-				<div class="clr"></div>
 			</div>
 		</div>
-		<div id="copyright">
-			<?php $joomla= '<a href="http://www.joomla.org">Joomla!&#174;</a>';
-			echo JText::sprintf('JGLOBAL_ISFREESOFTWARE', $joomla) ?>
+		<!-- Container -->
+		<div class="container">
+			<?php echo JHtml::_('installation.stepbar'); ?>
+			<jdoc:include type="installation" />
+			<hr />
+			<div class="footer">
+				<p class="pull-right"><a href="#top" id="back-top">Back to top</a></p>
+				<p><?php $joomla= '<a href="http://www.joomla.org">Joomla!&#174;</a>';
+				echo JText::sprintf('JGLOBAL_ISFREESOFTWARE', $joomla) ?></p>
+			</div>
 		</div>
 	</body>
 </html>
