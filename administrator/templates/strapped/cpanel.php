3a9a5844c2a61bee58e0a734ee09e4709abc70f9
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-03-31 16:29:30 -0500

    - updating copyright info & loading index.php instead of having duplicate layouts
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/templates/strapped/cpanel.php
+++ administrator/templates/strapped/cpanel.php
@@ -1,6 +1,13 @@
 <?php
-	/*** @copyright Copyright Pixel Praise LLC © 2012. All rights reserved. */
-	// no direct access
-	defined('_JEXEC') or die;
-	require_once(dirname(__FILE__).DS.'index.php');
+/**
+ * @package		Joomla.Administrator
+ * @subpackage	Templates.strapped
+ * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @since		3.0
+ */
+ 
+// no direct access
+defined('_JEXEC') or die;
+require_once JPATH_ADMINISTRATOR.'/templates/'.$this->template.'/index.php';
 ?>
