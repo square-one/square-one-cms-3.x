32e095b68275cc4b5f616737e1310aa797b908d2
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-03-31 17:18:08 -0500

    - removing sliders and updating to Bootstrap well for module style
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/components/com_cpanel/views/cpanel/tmpl/default.php
+++ administrator/components/com_cpanel/views/cpanel/tmpl/default.php
@@ -11,22 +11,9 @@ defined('_JEXEC') or die;
 ?>
 
 <?php
-echo JHtml::_('sliders.start', 'panel-sliders', array('useCookie'=>'1'));
-
 foreach ($this->modules as $module) {
-	$output = JModuleHelper::renderModule($module);
+	$output = JModuleHelper::renderModule($module, array('style' => 'well'));
 	$params = new JRegistry;
 	$params->loadString($module->params);
-	if ($params->get('automatic_title', '0')=='0') {
-		echo JHtml::_('sliders.panel', $module->title, 'cpanel-panel-'.$module->name);
-	}
-	elseif (method_exists('mod'.$module->name.'Helper', 'getTitle')) {
-		echo JHtml::_('sliders.panel', call_user_func_array(array('mod'.$module->name.'Helper', 'getTitle'), array($params)), 'cpanel-panel-'.$module->name);
-	}
-	else {
-		echo JHtml::_('sliders.panel', JText::_('MOD_'.$module->name.'_TITLE'), 'cpanel-panel-'.$module->name);
-	}
 	echo $output;
 }
-
-echo JHtml::_('sliders.end');
