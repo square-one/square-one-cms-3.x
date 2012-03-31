c085e54f43224ed58e3625787e969bc42183d308
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-03-31 16:29:06 -0500

    - Bootstrapping login
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/modules/mod_login/tmpl/default.php
+++ administrator/modules/mod_login/tmpl/default.php
@@ -10,29 +10,38 @@
 defined('_JEXEC') or die;
 JHtml::_('behavior.keepalive');
 ?>
-<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="form-login">
+<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="form-login" class="form-horizontal">
 	<fieldset class="loginform">
-
-				<label id="mod-login-username-lbl" for="mod-login-username"><?php echo JText::_('JGLOBAL_USERNAME'); ?></label>
-				<input name="username" id="mod-login-username" type="text" class="inputbox" size="15" />
-
-				<label id="mod-login-password-lbl" for="mod-login-password"><?php echo JText::_('JGLOBAL_PASSWORD'); ?></label>
-				<input name="passwd" id="mod-login-password" type="password" class="inputbox" size="15" />
-
-				<label id="mod-login-language-lbl" for="lang"><?php echo JText::_('MOD_LOGIN_LANGUAGE'); ?></label>
-				<?php echo $langs; ?>
-
-				<div class="button-holder">
-					<div class="button1">
-						<div class="next">
-							<a href="#" onclick="document.getElementById('form-login').submit();">
-								<?php echo JText::_('MOD_LOGIN_LOGIN'); ?></a>
-						</div>
-					</div>
-				</div>
-
-		<div class="clr"></div>
-		<input type="submit" class="hidebtn" value="<?php echo JText::_( 'MOD_LOGIN_LOGIN' ); ?>" />
+		<legend><?php echo JText::_('MOD_LOGIN_LOGIN'); ?></legend>
+		<div class="control-group">
+			<label id="mod-login-username-lbl" class="control-label" for="mod-login-username"><?php echo JText::_('JGLOBAL_USERNAME'); ?></label>
+			<div class="controls">
+			  <div class="input-prepend">
+			    <span class="add-on"><i class="icon-user"></i></span><input name="username" id="mod-login-username" type="text" class="inputbox" size="15" />
+			  </div>
+			</div>
+		</div>
+		<div class="control-group">
+			<label id="mod-login-password-lbl" class="control-label" for="mod-login-password"><?php echo JText::_('JGLOBAL_PASSWORD'); ?></label>
+			<div class="controls">
+			  <div class="input-prepend">
+			    <span class="add-on"><i class="icon-lock"></i></span><input name="passwd" id="mod-login-password" type="password" class="inputbox" size="15" />
+			  </div>
+			</div>
+		</div>
+		<div class="control-group">
+			<label id="mod-login-language-lbl" class="control-label" for="lang"><?php echo JText::_('MOD_LOGIN_LANGUAGE'); ?></label>
+			<div class="controls">
+			  <div class="input-prepend">
+			    <span class="add-on"><i class="icon-comment"></i></span><?php echo $langs; ?>
+			  </div>
+			</div>
+		</div>		
+		<div class="control-group">
+			<div class="controls">
+				<input type="submit" class="btn btn-primary btn-large" value="<?php echo JText::_( 'MOD_LOGIN_LOGIN' ); ?>" />
+			</div>
+		</div>
 		<input type="hidden" name="option" value="com_login" />
 		<input type="hidden" name="task" value="login" />
 		<input type="hidden" name="return" value="<?php echo $return; ?>" />
