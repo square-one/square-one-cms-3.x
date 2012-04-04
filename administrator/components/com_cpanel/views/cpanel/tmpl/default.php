3e9096a17dfe337e8aca433ec746fddb3a6eb5e0
Kyle Ledbetter <kyle@kyleledbetter.com>
2012-04-04 11:41:38 -0500

    - adding left nav to com_cpanel
    - TODO: make this dynamic so extensions can integrate
    
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- administrator/components/com_cpanel/views/cpanel/tmpl/default.php
+++ administrator/components/com_cpanel/views/cpanel/tmpl/default.php
@@ -8,12 +8,41 @@
 
 // no direct access
 defined('_JEXEC') or die;
-?>
 
-<?php
-foreach ($this->modules as $module) {
-	$output = JModuleHelper::renderModule($module, array('style' => 'well'));
-	$params = new JRegistry;
-	$params->loadString($module->params);
-	echo $output;
-}
+$user = JFactory::getUser();
+$lang = JFactory::getLanguage();
+?>
+<div class="row-fluid">
+	<div class="span3">
+		<div class="sidebar-nav">
+			<ul class="nav nav-list">
+              <li class="nav-header">Submenu</li>
+              <li class="active"><a href="<?php echo $this->baseurl; ?>">Dashboard</a></li>
+              <li class="nav-header">System</li>
+              <?php if($user->authorise('core.admin')):?>
+              	<li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_config">Global Configuration</a></li>
+              	<li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_admin&view=sysinfo">System Information</a></li>
+              <?php endif;?>
+              <?php if($user->authorise('core.manage', 'com_cache')):?>
+             	 <li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_cache">Clear Cache</a></li>
+              <?php endif;?>
+              <?php if($user->authorise('core.admin', 'com_checkin')):?>
+              	<li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_checkin">Global Check-in</a></li>
+              <?php endif;?>
+              <?php if($user->authorise('core.manage', 'com_installer')):?>
+             	 <li><a href="<?php echo $this->baseurl; ?>/index.php?option=com_installer">Install Extensions</a></li>
+              <?php endif;?>
+            </ul>
+        </div>
+	</div>
+	<div class="span9">
+	<?php
+	foreach ($this->modules as $module) {
+		$output = JModuleHelper::renderModule($module, array('style' => 'well'));
+		$params = new JRegistry;
+		$params->loadString($module->params);
+		echo $output;
+	}
+	?>
+	</div>
+</div>
