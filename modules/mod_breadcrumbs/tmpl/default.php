0fb710b679352f04c78638133442e39b57b1dbd8
Kyle Ledbetter <kledbetter@ebay.com>
2012-04-29 12:45:03 -0500

    - Bootstrapping mod_breadcrumbs
    Signed-off-by: Kyle Ledbetter <kledbetter@ebay.com>

--- modules/mod_breadcrumbs/tmpl/default.php
+++ modules/mod_breadcrumbs/tmpl/default.php
@@ -10,15 +10,16 @@
 defined('_JEXEC') or die;
 ?>
 
-<div class="breadcrumbs<?php echo $moduleclass_sfx; ?>">
+<ul class="breadcrumb <?php echo $moduleclass_sfx; ?>">
 <?php if ($params->get('showHere', 1))
 	{
-		echo '<span class="showHere">' .JText::_('MOD_BREADCRUMBS_HERE').'</span>';
+		echo '<li class="active">' .JText::_('MOD_BREADCRUMBS_HERE').' </li>';
 	}
 ?>
 <?php for ($i = 0; $i < $count; $i ++) :
 
 	// If not the last item in the breadcrumbs add the separator
+	echo '<li>';
 	if ($i < $count -1) {
 		if (!empty($list[$i]->link)) {
 			echo '<a href="'.$list[$i]->link.'" class="pathway">'.$list[$i]->name.'</a>';
@@ -28,15 +29,16 @@ defined('_JEXEC') or die;
 			echo '</span>';
 		}
 		if($i < $count -2){
-			echo ' '.$separator.' ';
+			echo '<span class="divider">/</span>';
 		}
 	}  elseif ($params->get('showLast', 1)) { // when $i == $count -1 and 'showLast' is true
 		if($i > 0){
-			echo ' '.$separator.' ';
+			echo '<span class="divider">/</span>';
 		}
 		 echo '<span>';
 		echo $list[$i]->name;
 		  echo '</span>';
 	}
+	echo '<li>';
 endfor; ?>
-</div>
+</ul>
